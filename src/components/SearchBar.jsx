51b01f6e39ad866f8e6a957f626ced87dd96e8cf
MaciejBascik <96304157+MaciejBascik@users.noreply.github.com>
2024-03-15 22:09:31 +0100

    add: initial files

--- src/components/SearchBar.jsx
+++ src/components/SearchBar.jsx
@@ -11,7 +11,7 @@ const SearchBar = () => {
     e.preventDefault();
 
     if (searchTerm) {
-      navigate(`/search/${searchTerm}`);
+      navigate(`youtube-Clone/search/${searchTerm}`);
 
       setSearchTerm('');
     }
